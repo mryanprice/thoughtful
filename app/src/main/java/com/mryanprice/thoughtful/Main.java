3716428cf595ae378fcbff5746112c63559639c8
Ryan Price <mryanprice@gmail.com>
2025-08-11 12:30:13 -0700

    clean up auto gen comments

--- app/src/main/java/com/mryanprice/thoughtful/Main.java
+++ app/src/main/java/com/mryanprice/thoughtful/Main.java
@@ -3,8 +3,6 @@ package com.mryanprice.thoughtful;
 import java.math.BigInteger;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         try {
